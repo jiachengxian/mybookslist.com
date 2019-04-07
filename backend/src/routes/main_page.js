40cd9575c647680ed73a68a2c4ca347cfd1344ee
jiachengxian <nexoph@gmail.com>
2019-04-07 00:22:37 -0700

    refined Book page - added additional fields, separate function for rendering authors div, updated css for authors div, and backend changes to specify book title

--- backend/src/routes/main_page.js
+++ backend/src/routes/main_page.js
@@ -3,8 +3,8 @@ const BOOK_COLLECTION_NAME = 'Books';
 
 module.exports = function handle_main_page_routes(router,db){
     //handle get request on main page
-    router.route('/get_book_json/default').get(function(req,res){
-        db.db(STATIC_DB_NAME).collection(BOOK_COLLECTION_NAME).find({"Title":{$exists:true}}).toArray()
+    router.route('/get_book_json/:title').get(function(req,res){
+        db.db(STATIC_DB_NAME).collection(BOOK_COLLECTION_NAME).find({"Title":req.params['title']}).toArray()
         .then((docs)=>{
             //for each book do x
             docs.forEach(function(doc){
--- frontend/src/Book.js
+++ frontend/src/Book.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import "./Book.css";
-import testImage from "./testImage.jpg";
 
 class Book extends Component{
     constructor(props){
@@ -13,6 +12,7 @@ class Book extends Component{
         };
         this.showDetails = this.showDetails.bind(this);
         this.detailsDiv = this.detailsDiv.bind(this);
+        this.authorsDiv = this.authorsDiv.bind(this);
     }
 
     showDetails(e) {
@@ -25,10 +25,14 @@ class Book extends Component{
         if(this.state.details){
             return( 
                 <div>
-                    <div id="isbn">ISBN: {this.state.book_data.ISBN[0]}</div>
-                    <div id="publisher">Publisher: {this.state.book_data.Publisher}</div>
-                    <div id="language">Language: </div>
-                    <div id="release">Publish Date: {this.state.book_data.Publish_Date}</div>
+                    {this.state.book_data.ISBN!=null &&
+                    <div id="isbn">ISBN: {this.state.book_data.ISBN}</div>}
+                    {this.state.book_data.Publisher!=null &&
+                    <div id="publisher">Publisher: {this.state.book_data.Publisher}</div>}
+                    {this.state.book_data.Publish_Date!=null &&
+                    <div id="release">Publish Date: {this.state.book_data.Publish_Date}</div>}
+                    {this.state.book_data.Pages!=null &&
+                    <div id="pages">Pages: {this.state.book_data.Pages}</div>}
                     <a id="less-details" className="details-button" onClick={this.showDetails} href="#">Less details...</a>
                 </div>
                 );
@@ -41,8 +45,35 @@ class Book extends Component{
         }
     }
 
+    authorsDiv(){
+        if(this.state.book_data != null && this.state.book_data.Author != null){
+            return(
+                <div className="author_block">by:&nbsp;
+                    {/*<a href="author.html">{this.state.book_data.Author}</a>*/}
+                    {this.state.book_data.Author.map((author,i) => {
+                        if(i+1 === this.state.book_data.Author.length){
+                            return(
+                                <div className="author_container">
+                                <a href="">{author}</a>
+                                </div>  
+                            );
+                        }else{
+                            return(
+                                <div className="author_container">
+                                    <a href="">{author}</a>
+                                ,&nbsp;</div>    
+                            ); 
+                        }
+                    })}
+                </div>
+            );
+        }else{
+            return(null)
+        }
+    }
+
     componentDidMount(){
-        axios.get('http://localhost:8000/get_book_json/default')
+        axios.get('http://localhost:8000/get_book_json/The Yale Shakespeare Complete Works')
         .then(response=>{
             //console.log(response.data);
             this.setState({book_data:response.data});
@@ -52,7 +83,7 @@ class Book extends Component{
 
     render() {
         var DetailDiv = this.detailsDiv;
-
+        var AuthorsDiv = this.authorsDiv;
         return(
             <div>
                 <div className="row" id="basic-info-row">
@@ -65,7 +96,12 @@ class Book extends Component{
                     <div className="col">
                         <div id="text">
                             <h1 id="title">{this.state.book_data.Title}</h1>
-                            <div id="author">by: <a href="author.html">{this.state.book_data.Author}</a></div>
+                            {/*<a href="author.html">{this.state.book_data.Author}</a>*/}
+                            <AuthorsDiv></AuthorsDiv>
+                            {this.state.book_data.Series!="" &&
+                            <div id="series">Series: 
+                                <a id="series-link" href=""> {this.state.book_data.Series}</a>
+                            </div>}
                             <DetailDiv></DetailDiv>
                             
                         </div> 
