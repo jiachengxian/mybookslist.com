9304246b4a94371ac66dcd445ee7437c805771e4
Jia Cheng Xian <nexoph@gmail.com>
2019-06-01 01:19:54 -0700

    dynamic routing set up for books and authors
    -updated Book preview links to redirect to url based on book title name
    
    -created dummy landing pages for main Authors and Books pages.

---
+++ frontend/src/author-page-component/AuthorMainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AuthorMainPage extends Component {
+    render(){
+        return(
+            <div>
+                Author Main Page
+            </div>
+        )
+    }
+}
+
+export default AuthorMainPage;
---
+++ frontend/src/book-page-component/BookMainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class BookMainPage extends Component {
+    render(){
+        return(
+            <div>
+                Book Main Page
+            </div>
+        )
+    }
+}
+
+export default BookMainPage;
