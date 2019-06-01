367b545b4b5751f0528245165600e384d895a5aa
Damon Chadic <damonchadic@gmail.com>
2019-06-01 14:38:04 -0700

    basic results page/routing

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -6,6 +6,7 @@ import Book from "./book-page-component/Book";
 import BookMainPage from "./book-page-component/BookMainPage";
 import Author from "./author-page-component/Author";
 import AuthorMainPage from "./author-page-component/AuthorMainPage";
+import Results from "./search-page-component/Results"
 class Main extends Component {
     render() {
         return(
@@ -27,6 +28,7 @@ class Main extends Component {
                             <Route path="/author_landing_page" render={ () => <AuthorMainPage></AuthorMainPage>}/>
                             <Route path={"/book/:name"} render={({match}) => <Book name={`${match.params.name}`}></Book>}/>
                             <Route path={"/author/:name"} render={({match}) => <Author name={`${match.params.name}`}></Author>}/>
+                            <Route path={"/search"} render={() => <Results ></Results>}/>
                         </Switch>
                     </div>
                     <div className="col dummy"></div>      
