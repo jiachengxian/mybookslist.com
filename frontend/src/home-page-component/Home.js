734e895006266196f269bd76379fe9da16d2ec6c
Jia Cheng Xian <nexoph@gmail.com>
2019-06-07 01:18:17 -0700

    basic autosuggest functionality

--- frontend/src/home-page-component/Home.js
+++ frontend/src/home-page-component/Home.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import "./Home.css";
-import Book from "../book-page-component/Book"
-import axios from 'axios';
-import BookPreview from "../book-page-component/BookPreview";
 import BookPreviewList from "../book-page-component/BookPreviewList"
+import SearchBar from "../search-bar-component/SearchBar";
 import logo from './placeholder.gif'
 
 class Home extends Component {
@@ -11,26 +9,11 @@ class Home extends Component {
         super(props);
         
         this.state = {
-            changed: 0,
-            search: ""
         };
-        this.setSearch = this.setSearch.bind(this);
-        this.changeQ = this.changeQ.bind(this);
  
     }
 
-    setSearch(e) {
-        this.setState({
-            search: e.target.value
-        });
-    }
-
-    changeQ(e){
-        alert(this.state.search);
-    }
-
     render(props){
-        var title = this.state.search;
         var titles = [{"Title":"Harry Potter and the Prisoner of Azkaban"},{"Title":"Harry Potter and the Chamber of Secrets"},{"Title":"Harry Potter and the Order of the Phoenix"},{"Title":"Harry Potter and the Goblet of Fire"},{"Title":"Harry Potter and the Sorcerer's Stone"},{"Title":"Harry Potter and the Half-Blood Prince"},{"Title":"Harry Potter and the Deathly Hallows"}];
         return(
             <div className="home-header">
@@ -39,15 +22,11 @@ class Home extends Component {
                 </div>
 
                 <div className="row">
-                    <form className="form-inline search" action={`/search/${this.state.search}`}>
-                        <input className="form-control m-2" onChange={this.setSearch}></input>
-                        <button className="btn searchbutton" type="submit">go</button>
-                    </form>
+                    <SearchBar></SearchBar>
                 </div>
 
                 <hr></hr>
                     <BookPreviewList book_list={titles}></BookPreviewList>
-                <h1>{title}</h1>
             </div>
         )
     }
---
+++ frontend/src/search-bar-component/SearchBar.js
@@ -0,0 +1,98 @@
+import Autosuggest from 'react-autosuggest';
+import React, { Component } from "react";
+import axios from 'axios';
+import GLOBALS from '../globals';
+import "./SearchBar.css";
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.Title;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <span className="suggestionContainer">
+    {suggestion.Title}
+  </span>
+);
+
+class SearchBar extends Component {
+  constructor() {
+    super();
+
+    // Autosuggest is a controlled component.
+    // This means that you need to provide an input value
+    // and an onChange handler that updates this value (see below).
+    // Suggestions also need to be provided to the Autosuggest,
+    // and they are initially empty because the Autosuggest is closed.
+    this.state = {
+      value: '',
+      suggestions: []
+    };
+  }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    const maxNumBooksDisplayed = 5
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+    axios.get(`${GLOBALS.BASE_URL}/${GLOBALS.SEARCH_FOR_BOOKS_PATH}/${inputValue}/${maxNumBooksDisplayed}`)
+      .then(response=>{
+        var suggestions = inputLength === 0 ? [] : response.data.filter(book =>
+          book.Title.toLowerCase().includes(inputValue));
+        this.setState({
+            suggestions: suggestions
+        });
+      })
+      .catch(console.error);
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+
+    // Autosuggest will pass through all these props to the input.
+    const inputProps = {
+      placeholder: 'Type a book name, author...',
+      value,
+      onChange: this.onChange
+    };
+
+    // Finally, render it!
+    return (
+      <form className="form-inline search" action={`/search/${this.state.value}`}>
+        <Autosuggest className="form-control m-2"
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+          theme={{
+            suggestionsContainerOpen:{
+              'position':'absolute',
+              'z-index':3,
+            }
+            ,
+          }}
+        />
+        <button className="btn searchbutton" type="submit">go</button>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
